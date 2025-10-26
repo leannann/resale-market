29a00648f0072132ac274c9eece51529f8249da1
leannann <terzi_andrey_sergeevich@mail.ru>
2025-10-26 22:37:51 +0300

    feat: добавить UsersController

---
+++ src/main/java/ru/skypro/homework/controller/UsersController.java
@@ -0,0 +1,44 @@
+package ru.skypro.homework.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.skypro.homework.dto.NewPassword;
+import ru.skypro.homework.dto.UpdateUser;
+import ru.skypro.homework.dto.User;
+
+@Slf4j
+@CrossOrigin(value = "http://localhost:3000")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UsersController {
+
+    @PostMapping("/set_password")
+    public ResponseEntity<Void> setPassword(@RequestBody NewPassword newPassword) {
+        log.info("Password update requested: {}", newPassword);
+        return ResponseEntity.ok().build(); // заглушка
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<User> getUser() {
+        log.info("Get current user info");
+        return ResponseEntity.ok(new User());
+    }
+
+    @PatchMapping("/me")
+    public ResponseEntity<UpdateUser> updateUser(@RequestBody UpdateUser updateUser) {
+        log.info("Update current user data: {}", updateUser);
+        return ResponseEntity.ok(updateUser != null ? updateUser : new UpdateUser());
+    }
+
+    @PatchMapping(value = "/me/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Void> updateUserImage(@RequestPart("image") MultipartFile image) {
+        log.info("Update current user image: {}", image.getOriginalFilename());
+        return ResponseEntity.ok().build();
+    }
+}
